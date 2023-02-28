58ccb32f1ce0a538eb11209d4ff190600c0d2d2f
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-28 16:42:32 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component'; 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+production: true,
+
+firebase : {
+    apiKey: "AIzaSyC6YsxbAR8XGn4cWJu4q-yIMy2vSG6jBEo",
+    authDomain: "student-management-syste-639d4.firebaseapp.com",
+    projectId: "student-management-syste-639d4",
+    storageBucket: "student-management-syste-639d4.appspot.com",
+    messagingSenderId: "432738586248",
+    appId: "1:432738586248:web:ee951c51bd89ebc771d93d"
+  }
+  
+
+};
